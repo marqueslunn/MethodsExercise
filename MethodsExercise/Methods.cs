d47cd09a7a553ec435ac8d0454851e808d60bae0
Marques Lunn <cmarqueslunn@gmail.com>
2023-02-01 15:02:24 -0500

    MethodEx

---
+++ MethodsExercise/Methods.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace MethodsExercise
+{
+    public class Methods
+    {
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+
+        }
+
+        public static int Sub(int num1, int num2)
+        {
+            return num1 - num2;
+
+
+
+
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+
+        }
+
+        public static int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+
+        }
+
+        public static int Mod(int num1, int num2)
+        {
+            return num1 % num2;
+
+        }
+    }
+}
+
--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            string color = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite Animal?");
+            string favoriteAnimal = Console.ReadLine();
+
+            Console.WriteLine("What is the name of your favorite band?");
+            string favoriteBand = Console.ReadLine();
+
+            Console.WriteLine($"Hello {name} I hope all is well with you! I heard" +
+                $" your favorite color was a {color}. I heard your favorite animal " +
+                $" was {favoriteAnimal}. I also heard you listened to {favoriteBand}");
+
+
+
+            Methods.Add(23, 66);
+            Console.WriteLine(Methods.Add(23, 66));
+
+            Console.WriteLine(Methods.Sub(23, 66));
+            Console.WriteLine(Methods.Multiply(23, 66));
+            Console.WriteLine(Methods.Divide(23, 66));
+            Console.WriteLine(Methods.Mod(23, 66));
+
+
         }
     }
 }
